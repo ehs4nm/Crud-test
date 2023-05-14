d5cfcb403a989b98af1cc3f823ffffbcc6926227
ehsan mohiti <mohiti.ehsan@gmail.com>
2023-05-14 12:52:18 +0330

    make last name  nullable

--- database/migrations/2023_05_14_073000_create_customers_table.php
+++ database/migrations/2023_05_14_073000_create_customers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('first_name', 60)->nullable();
-            $table->string('last_name', 60);
+            $table->string('last_name', 60)->nullable();
             $table->date('date_of_birth')->nullable();
             $table->string('phone_number', 20)->unique();
             $table->string('email', 100)->unique();
